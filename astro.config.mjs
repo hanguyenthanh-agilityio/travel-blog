2d1cdcb80b5545f9b471407c632ecf97eaddde73
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-09-11 15:13:42 +0700

    Config Shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwindcss()],
+  integrations: [tailwindcss(), react()],
 });
