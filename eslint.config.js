3009c4e2fda2f460cd1bd45d737bbd727ef8b6f9
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-09-11 16:37:04 +0700

    feat: good message to test

--- eslint.config.js
+++ eslint.config.js
@@ -56,7 +56,6 @@ export default [
 
       // === Rules custom ===
       'no-console': 'warn',
-      'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
