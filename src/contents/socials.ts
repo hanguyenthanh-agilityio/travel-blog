8229152289f65fac7ce9cc3a87c5d3b8946ad4aa
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-09-15 15:07:08 +0700

    feat: implement social component

---
+++ src/contents/socials.ts
@@ -0,0 +1,27 @@
+export const socials = [
+  {
+    href: 'https://instagram.com',
+    label: 'Instagram',
+    icon: '/images/instagram.svg',
+  },
+  {
+    href: 'https://twitter.com',
+    label: 'Twitter',
+    icon: '/images/twitter.svg',
+  },
+  {
+    href: 'https://facebook.com',
+    label: 'Facebook',
+    icon: '/images/facebook.svg',
+  },
+  {
+    href: 'https://linkedin.com',
+    label: 'LinkedIn',
+    icon: '/images/linkedin.svg',
+  },
+  {
+    href: 'https://youtube.com',
+    label: 'YouTube',
+    icon: '/images/youtube.svg',
+  },
+];
