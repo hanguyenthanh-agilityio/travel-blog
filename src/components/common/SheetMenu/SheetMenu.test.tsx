0efcd48a07a42e3bebc6e805917ebca0b2c45d18
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-09-22 17:43:58 +0700

    feat: fix axe

--- src/components/common/SheetMenu/SheetMenu.test.tsx
+++ src/components/common/SheetMenu/SheetMenu.test.tsx
@@ -1,4 +1,3 @@
-// src/components/common/MobileSheetMenu/MobileSheetMenu.test.tsx
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
