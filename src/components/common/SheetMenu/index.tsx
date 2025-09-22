e0bcebf353cae40b206d2bd271ea51dbdb33d9e3
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-09-22 10:34:45 +0700

    feat: update config to deploy app

--- src/components/common/SheetMenu/index.tsx
+++ src/components/common/SheetMenu/index.tsx
@@ -8,6 +8,7 @@ import {
 import NavDropdownMenu from '@/components/common/NavDropdownMenu';
 import Hamburger from '@/components/icon/hamburger';
 import { Button } from '@/components/ui/button';
+
 const menu = [
   { href: '/', text: 'Home' },
   { href: '/blog', text: 'Blog' },
