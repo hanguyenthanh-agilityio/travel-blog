64127eaeb251244efc6ecb391625b7968b9d9fd8
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-09-23 16:06:02 +0700

    feat: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwindcss(), react()],
+  redirects: {
+    '/': 'page/1/',
+  },
   i18n: {
     locales: ['es', 'en', 'pt-br', 'vi'],
     defaultLocale: 'en',
