d6cec4088b628f43c079bab61046ddba8641034c
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-09-26 11:30:36 +0700

    fix: upgrade tailwind version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwindcss(), react()],
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   redirects: {
     '/': 'page/1/',
   },
