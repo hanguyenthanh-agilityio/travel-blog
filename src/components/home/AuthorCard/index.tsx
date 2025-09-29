22bcde051c47400f97bca2947f52213b9be118f1
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-09-29 14:27:04 +0700

    fix: update import cn in component

--- src/components/home/AuthorCard/index.tsx
+++ src/components/home/AuthorCard/index.tsx
@@ -5,6 +5,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 // Libs
 import type { Author } from '@/lib/schema';
+import { cn } from '@/lib/utils';
 
 export interface AuthorCardProps extends Author {
   dark?: boolean;
@@ -22,27 +23,31 @@ const AuthorCard: React.FC<AuthorCardProps> = ({
   avatar,
   date,
   dark = false,
-  className = '',
-  classContainer = '',
-  classAvatar = '',
+  className,
+  classContainer,
+  classAvatar,
   layout = 'card',
   isBlogDetail = false,
   isRole = false,
 }) => {
   return (
     <div
-      className={`flex ${
+      className={cn(
+        'flex gap-4 md:gap-0',
         layout === 'hero'
           ? 'flex-col md:flex-row md:justify-between md:items-center'
-          : 'flex-row items-center justify-between'
-      } gap-4 md:gap-0 ${classContainer}`}
+          : 'flex-row items-center justify-between',
+        classContainer,
+      )}
     >
       {/* Create date */}
       {!isBlogDetail && (
         <p
-          className={`${
-            layout === 'hero' ? 'order-1' : 'order-2'
-          } text-sm ${dark ? 'text-white' : 'text-gray-500'}`}
+          className={cn(
+            'text-sm',
+            layout === 'hero' ? 'order-1' : 'order-2',
+            dark ? 'text-white' : 'text-gray-500',
+          )}
         >
           {date instanceof Date ? date.toLocaleDateString() : date}
         </p>
@@ -50,20 +55,24 @@ const AuthorCard: React.FC<AuthorCardProps> = ({
 
       {/* Author info */}
       <div
-        className={`flex items-center gap-3 ${className} ${
-          layout === 'hero' ? 'order-2' : 'order-1'
-        }`}
+        className={cn(
+          'flex items-center gap-3',
+          layout === 'hero' ? 'order-2' : 'order-1',
+          className,
+        )}
       >
         <Avatar className={classAvatar}>
           <AvatarImage src={avatar} alt={name} />
           <AvatarFallback>{name?.[0]}</AvatarFallback>
         </Avatar>
+
         <div
-          className={`${dark ? 'text-white' : 'text-primary'} ${
+          className={cn(
             layout === 'hero'
               ? 'flex flex-col gap-1.5'
-              : 'flex flex-col items-center'
-          }`}
+              : 'flex flex-col items-center',
+            dark ? 'text-white' : 'text-primary',
+          )}
         >
           <p className="text-sm font-medium">{name}</p>
           {isRole && <p className="text-xs opacity-70">{role}</p>}
