45c092f60673b83d7368a972fe8fc84d0a65804a
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-10-01 14:56:52 +0700

    fix: update config astro critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import critters from 'astro-critters';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    critters({
+      preload: 'media',
+      pruneSource: true,
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
