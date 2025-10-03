5614e7a4852c1092cb01e8cf57047d7b6ea67c95
ha.nguyenthanh <ha.nguyenthanh@asnet.com.vn>
2025-10-03 10:49:53 +0700

    fix: user can click hamburger button

--- src/components/common/SheetMenu/index.tsx
+++ src/components/common/SheetMenu/index.tsx
@@ -10,7 +10,7 @@ import {
 import { NavDropdownMenu } from '@/components';
 import Hamburger from '@/components/icon/hamburger';
 import { Button } from '@/components/ui/button';
-import Link from '@/components/ui/link/index.astro';
+import Link from '@/components/ui/link';
 
 // Contents
 import { menu, otherPages } from '@/contents/blog';
@@ -21,7 +21,8 @@ export default function MobileSheetMenu() {
       <SheetTrigger asChild>
         <Button
           aria-label="Open menu"
-          className="p-2 rounded-md hover:bg-gray-100 bg-[none]"
+          variant="ghost"
+          className="p-2 !border-[none] rounded-[none] hover:bg-gray-100 bg-[none]"
         >
           <Hamburger />
         </Button>
