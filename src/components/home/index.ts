7f9d7ade1d91a47b9900e1335feecad5fb15238d
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-10-06 18:12:21 +0700

    fix: update import file

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,5 +1,4 @@
 export { default as AuthorCard } from './AuthorCard';
-export { default as BlogHero } from './BlogHero/index.astro';
 export { default as BlogGrid } from './BlogGrid/index.astro';
 export { default as SectionTitle } from './SectionTitle/index.astro';
 export { default as BlogCard } from './BlogCard/index.astro';
