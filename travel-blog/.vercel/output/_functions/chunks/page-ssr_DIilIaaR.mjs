772d6672884effb3e64fd5cb43de14f18b2f1768
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-10-20 15:08:57 +0700

    fix: remove prebuilt output for Vercel build

--- travel-blog/.vercel/output/_functions/chunks/page-ssr_DIilIaaR.mjs
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client';
-
-const sanityClient = createClient(
-            {"apiVersion":"2025-01-01","projectId":"d5zc7aiu","dataset":"production","useCdn":true,"stega":{"studioUrl":"\u002Fstudio"}}
-          );
-
-globalThis.sanityClient = sanityClient;
-
-export { sanityClient as s };
