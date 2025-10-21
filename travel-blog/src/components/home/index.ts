35740b68d0462e6d6e57a6758db804c366d2a17b
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-10-21 17:07:10 +0700

    fix: update structure components

--- travel-blog/src/components/home/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as AuthorCard } from './AuthorCard';
-export { default as BlogGrid } from './BlogGrid/index.astro';
-export { default as SectionTitle } from './SectionTitle/index.astro';
-export { default as BlogCard } from './BlogCard/index.astro';
-export { default as BlogPage } from './BlogPage/index.astro';
