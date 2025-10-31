65badcfa7b467309ca87c7a5a1738442a9a6779e
Ha Nguyen Thanh <ha.nguyenthanh@asnet.com.vn>
2025-10-31 11:30:47 +0700

    fix: update portableText for home page

--- travel-blog/src/queries/homePage.ts
+++ travel-blog/src/queries/homePage.ts
@@ -13,30 +13,36 @@ export const homePageQuery = `
       date
     }
   },
-  "popularPosts": *[_type == "post" && category == "popular"] | order(_createdAt desc)[0..2]{
-    _id,
-    title,
-    slug,
-    excerpt,
-    image{asset->{url}},
-    author->{
-      name,
-      role,
-      avatar{asset->{url}},
-      date
+  "popularPosts": {
+    "heading": *[_type == "sectionHeading" && section == "popular"][0].heading,
+    "posts": *[_type == "post" && category == "popular"] | order(_createdAt desc)[0..2]{
+      _id,
+      title,
+      slug,
+      excerpt,
+      image{asset->{url}},
+      author->{
+        name,
+        role,
+        avatar{asset->{url}},
+        date
+      }
     }
   },
-  "trendingPosts": *[_type == "post" && category == "trending"] | order(_createdAt desc)[0..5]{
-    _id,
-    title,
-    slug,
-    excerpt,
-    image{asset->{url}},
-    author->{
-      name,
-      role,
-      avatar{asset->{url}},
-      date
+   "trendingPosts": {
+    "heading": coalesce(*[_type == "sectionHeading" && section == "trending"][0].heading, []),
+    "posts": *[_type == "post" && category == "trending"] | order(_createdAt desc)[0..5]{
+      _id,
+      title,
+      slug,
+      excerpt,
+      image{asset->{url}},
+      author->{
+        name,
+        role,
+        avatar{asset->{url}},
+        date
+      }
     }
   }
 }
